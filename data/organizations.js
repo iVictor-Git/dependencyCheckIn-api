43560743d73acfb1b3ee760ed6a5bc5532bfa7c5
Victor Tran <victor.n.tran@outlook.com>
2018-05-19 21:48:35 -0400

    Added data for database

---
+++ data/organizations.js
@@ -0,0 +1,30 @@
+const orgs = [
+    {
+        name: 'Youth & Family Alternatives, INC.',
+        organization: 'Safe Children Coalition',
+        county: 'Manatee',
+        address: {
+            street: '5729 Manatee Ave. West',
+            street2: '',
+            city: 'Bradenton',
+            state: 'FL',
+            zip: '34209'
+        },
+        phone: '941200000'
+    },
+    {
+        name: 'CenterStone',
+        organization: 'Safe Children Coalition',
+        county: 'Manatee',
+        address: {
+            street: '5729 Manatee Ave. West',
+            street2: '',
+            city: 'Bradenton',
+            state: 'FL',
+            zip: '34209'
+        },
+        phone: '941200000'
+    }
+]
+
+export default orgs;
